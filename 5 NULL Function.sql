81e6b0404428853893e47cb8b803ff199c7d669c
Soungyoung Bark(Park) <43887824+soungyoung0723@users.noreply.github.com>
2020-01-31 09:42:56 -0600

    Update 5 NULL Function.sql

--- 5 NULL Function.sql
+++ 5 NULL Function.sql
@@ -5,3 +5,10 @@
 
 SELECT name, start_date, NVL2(end_date, 'end in two weeks', TO_CHAR(SYSDATE, 'dd-mm-YYYY')) AS "End date";
 FROM f_promotion_menus;
+                                                                    
+# Not all Global Fast Foods staff members receive overtime pay. Instead of displaying a null value for these employees, 
+# replace null with zero. 
+# Include the employee’s last name and overtime rate in the output. Label the overtime rate as “Overtime Status”.  
+           
+SELECT last_name, NVL(overtime_rate, 0) AS "Overtime Status"
+FROM f_staffs;
