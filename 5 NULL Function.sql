81fd299e92743a7d2bc3bb32795e1ea6758a1ad5
Soungyoung Bark(Park) <43887824+soungyoung0723@users.noreply.github.com>
2020-02-03 23:58:37 -0600

    Update 5 NULL Function.sql

--- 5 NULL Function.sql
+++ 5 NULL Function.sql
@@ -12,3 +12,23 @@ FROM f_promotion_menus;
            
 SELECT last_name, NVL(overtime_rate, 0) AS "Overtime Status"
 FROM f_staffs;
+
+# The manager of Global Fast Foods has decided to give all staff who currently do not earn overtime an overtime rate of $5.00. 
+# Construct a query that displays the last names and the overtime rate for each staff member, 
+# substituting $5.00 for each null overtime value.      
+                                                                    
+SELECT last_name, NVL(overtime_rate, 5) AS "Overtime Status"
+FROM f_staffs;
+                                                                    
+# Not all Global Fast Foods staff members have a manager.
+# Create a query that displays the employee last name and 9999 in the manager ID column for these employees. 
+
+SELECT last_name, NVL(manager_id, 9999) AS "Overtime Status"
+FROM f_staffs;
+                                                                    
+# What does this query on the Global Fast Foods table return                                                                    
+                                                                    
+SELECT COALESCE(last_name, to_char(manager_id)) as NAME  
+FROM f_staffs; 
+                                                                   
+                                                                    
